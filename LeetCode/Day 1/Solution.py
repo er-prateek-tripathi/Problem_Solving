a222a92c8ba7773e9784943c3b462e7ce7fe5178
PREDATOR <prateek772492@gmail.com>
2024-01-01 13:48:33 +0530

    Day 1

---
+++ LeetCode/Day 1/Solution.py
@@ -0,0 +1,14 @@
+def mergeAlternately(word1: str, word2: str) -> str:
+    merged_string = ""
+    i, j = 0, 0
+    while i < len(word1) or j < len(word2):
+        if i < len(word1):
+            merged_string += word1[i]
+            i += 1
+        if j < len(word2):
+            merged_string += word2[j]
+        j += 1
+    return merged_string
+
+
+print(mergeAlternately("abc", "pqr"))
